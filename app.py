a3a1703a988d0dbf18d8f67e723bb12efb3cd428
Swarnava Basu <basuswarnava2@gmail.com>
2020-09-10 16:58:27 +0530

    Done

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import carrier 
 
-phone_number = phonenumbers.parse("+919674871858")  
+phone_number = phonenumbers.parse("+917686962350")  
 print(geocoder.description_for_number(phone_number,  
                                       'en'))    
 print(carrier.name_for_number(phone_number, 
